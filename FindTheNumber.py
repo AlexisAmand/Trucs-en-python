1e77a161c337c8ee93692724d7d2e3350d81cee2
Alex <alexis.amand@gmail.com>
2023-10-18 18:04:06 +0200

    petite optimisation du code

--- FindTheNumber.py
+++ FindTheNumber.py
@@ -9,8 +9,7 @@ numberToFind  = random.randint(min,max)
 numberUser = 0
 nbCoup = 0
 while numberUser != numberToFind:
-    question = "Proposer une valeur entre 1 et 50 :"
-    numberUser = int(input(question))
+    numberUser = int(input(f"Proposer une valeur entre {min} et {max} :"))
     nbCoup = nbCoup + 1
     if numberUser < numberToFind:
         print("trop petit !")
