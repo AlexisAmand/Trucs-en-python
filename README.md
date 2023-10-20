2366fb11bb02db40d2a985a2937186ca3c515cd1
Alex <alexis.amand@gmail.com>
2023-10-20 13:34:01 +0200

    Tuto #2 de Graven

---
+++ README.md
@@ -0,0 +1,16 @@
+J'apprends Python doucement...
+
+## Find the number
+
+Seras-tu capable de trouver à quel nombre pense l'ordinateur ?    
+https://github.com/AlexisAmand/python/blob/master/FindTheNumber.py
+
+## Les tutos de Graven
+Sa chaine Youtube :    
+https://www.youtube.com/@Gravenilvectuto
+
+Ma version de ses tps :
+
+
+
+
---
+++ TP Graven/tp-graven-02.py
@@ -0,0 +1,12 @@
+﻿# Tuto 02 de Graven
+# récolter une valeur porte monnaie
+porte_monnaie = int(input("Quelle est la valeur du porte monnaie ?"))
+print(f"Vous avez {str(porte_monnaie)} euros.")
+
+# créer un produit qui aura pour valeur 50
+prix_produit = 50
+print(f"Le produit coûte {str(prix_produit)} euros.")
+
+# afficher la nouvelle valeur du porte monnaie, après son achat
+porte_monnaie -= prix_produit
+print(f"Vous avez maintenant {str(porte_monnaie)} euros.")
