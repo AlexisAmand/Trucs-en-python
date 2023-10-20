41ad0eb57d82579ecfc1cf3a6c898c281d895da2
Alex <alexis.amand@gmail.com>
2023-10-20 11:46:35 +0200

    [update] ajout de 3 niveaux de difficulté

--- FindTheNumber.py
+++ FindTheNumber.py
@@ -1,24 +1,29 @@
 import random
 
-# valeur minimale et maximale du nombre à trouver
-min, max  = 1, 50
+# Valeur minimale du nombre à trouver
+min = 1
 
-# valeur aléatoire du nombre à trouver
+# Niveau de difficulté
+level = int(input(f"Niveau de difficulté (1/2/3) : "))
+max = level * 25
+
+# Valeur aléatoire du nombre à trouver
 numberToFind  = random.randint(min,max)
 
 numberUser = 0
 nbCoup = 0
+
+# Début de la partie
+print("Je pense à un nombre...")
+
 while numberUser != numberToFind:
     numberUser = int(input(f"Proposer une valeur entre {min} et {max} :"))
     nbCoup = nbCoup + 1
     if numberUser < numberToFind:
-        print("trop petit !")
+        print("Le nombre est plus grand !")
     elif numberUser > numberToFind:
-        print("trop grand !")
-   
-print("Vous avez trouvé en", nbCoup , "coup(s).")
+        print("Le nombre est plus petit !")
 
-
-
-
-
+# Le joueur a gagné !
+print(f"Bravo ! Le nombre à trouver était {numberToFind}.")
+print(f"Vous avez trouvé en {nbCoup} coup(s).")
