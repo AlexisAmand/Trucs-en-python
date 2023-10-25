77cf4d5efe891f90f4a523dc6e4f63cb0a3c10d7
Alex <alexis.amand@gmail.com>
2023-10-25 14:59:04 +0200

    [script] premier

---
+++ Maths/premier.py
@@ -0,0 +1,31 @@
+﻿# Ce nombre est-il premier ?
+
+# J'ai adapté mon script qui recherche les divisieurs
+
+def diviseurs(x):
+    divs = list()
+    for i in range (1, x+1):
+        if x%i == 0:
+            divs.append(i)
+    return divs
+
+# Uniquement pour voir si le test est fini
+
+fini = False
+
+# On demande à un entier à l'utilisateur
+# Il faut vérifier que x est bien une série de chiffres avec isdigit()
+
+# Un nombre N est premier s'il a uniquement 2 diviseurs : 1 et N
+        
+while fini == False:
+    x = input("Indiquez un entier: ")
+    if(x.isdigit()):
+        if len(diviseurs(int(x))) == 2:
+            print(f"{x} est premier !")
+            fini = True
+        else:
+            print(f"{x} n'est pas premier !")
+            fini = True
+    else:
+        print("Merci d'indiquez un nombre entier !")
--- README.md
+++ README.md
@@ -5,6 +5,7 @@ J'apprends Python doucement...
 ## Mathématiques
 
 * [Calcul de la factorielle d'un nombre](https://github.com/AlexisAmand/python/blob/master/Maths/factorielle.py)
+* {Déterminer si un entier est premier](https://github.com/AlexisAmand/python/blob/master/Maths/premier.py)
 * [Le mot est-il un palindrome ?](https://github.com/AlexisAmand/python/blob/master/Maths/palindrome.py)
 * [Liste des diviseurs d'un nombre entier](https://github.com/AlexisAmand/python/blob/master/Maths/diviseurs.py)
 * [Une année est-elle bissextile ?](https://github.com/AlexisAmand/python/blob/master/Maths/bissextile.py)
