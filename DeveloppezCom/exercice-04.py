27eab46ae27652b8dd01aa5f5bce4bd5321a9f4d
Alex <alexis.amand@gmail.com>
2023-10-27 16:46:27 +0200

    [dvp.com] exercice #04

---
+++ DeveloppezCom/exercice-04.py
@@ -0,0 +1,12 @@
+﻿# tuto dvp.com - exercice 4
+
+n = int(input("Entrez un nombre: "))
+
+while n < 1:
+    print("Il doit être positif et non nul !")
+    n = int(input("Entrez un nombre: "))
+
+if n%2 == 0:
+    print("PAIR")
+else:
+    print("IMPAIR")
