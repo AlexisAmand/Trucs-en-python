b14fdd128baafbdcae6bfebfe0ad47c2be654230
Alex <alexis.amand@gmail.com>
2023-10-31 11:09:46 +0100

    [dvp.com] exercice #08

---
+++ DeveloppezCom/exercice-08.py
@@ -0,0 +1,14 @@
+﻿# tuto dvp.com - exercice 8
+
+# fonction qui fait le calcul
+def hauteurParcourue(nombre, hauteur):
+    distance = 7 * 5 * 2 * nombre * hauteur
+    print(f"Pour {nombre} marches de {hauteur} cm, il parcourt {distance/100} m par semaine.")
+
+
+
+# programme principal
+nombreMarches = int(input("Combien de marches ?"))
+hauteurMarche = float(input("Hauteur d'une marche (cm) ?"))
+
+hauteurParcourue(nombreMarches, hauteurMarche)
