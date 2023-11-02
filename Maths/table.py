958e4b8843b209dc9cbe8c9fdeeb4cbe1203fbd2
Alex <alexis.amand@gmail.com>
2023-11-02 15:41:30 +0100

    [update] table
    Ajout d'une vérification, dans le cas où la valeur entrée par l'utilisateur n'est pas un entier.

--- Maths/table.py
+++ Maths/table.py
@@ -5,17 +5,21 @@ def table(m):
     for t in range (1, 11):
         print(f"{t} x {m} = {t * m}")
         
+print("Tables de multiplication\n")
+print("Ce programme affiche la table de N.\n")
 
 # programme principal
 while True:
-        m = int(input("Quelle table voulez vous afficher (0 pour quitter)? ")) 
+        m = input("\nQuelle table voulez vous afficher (0 pour quitter)? ")
         try:
+            m = int(m)
             if m == 0:
                 print("Au revoir.")
                 break
+            print(f"\nVoici la table de {5}:\n")
             table(m)
         except ValueError:
-            print("\nVous devez indiquer un entier !\n")
+            print("N doit être un entier positif.")
         
 
 
