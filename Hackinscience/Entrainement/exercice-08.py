09143a2c0772d8af253570a21dfe229b1cbdbf46
Alex <alexis.amand@gmail.com>
2023-11-22 11:15:10 +0100

    Update exercice-08.py

--- Hackinscience/Entrainement/exercice-08.py
+++ Hackinscience/Entrainement/exercice-08.py
@@ -12,4 +12,4 @@ heure = aujourdhui.strftime("%H")
 minute = aujourdhui.strftime("%M")
 seconde =aujourdhui.strftime("%S")
 
-print(f"Today is {annee}-{mois}-{jour} and it is {heure}-{minute}-{seconde}")
+print(f"Today is {annee}-{mois}-{jour} and it is {heure}:{minute}:{seconde}.")
