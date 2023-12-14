d3285ca524b10cb36ac7565c44dfcc25c1d02225
Alex <alexis.amand@gmail.com>
2023-12-14 11:46:05 +0100

    [hackinscience] des emojis en forme de coeur

---
+++ Hackinscience/Realiste/exercice-14.py
@@ -0,0 +1,11 @@
+﻿# Des emojis en forme de coeur
+
+import unicodedata
+
+for c in range(1, 230000):
+    try:
+        nom = unicodedata.name(chr(c))
+        if "HEART" in nom:
+            print(chr(c))
+    except ValueError:
+        continue
