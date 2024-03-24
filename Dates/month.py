3efce58d4a73568db471e3bd4acb664306155578
Alex <alexis.amand@gmail.com>
2024-03-24 15:10:12 +0100

    [update] calendrier du mois en cours

--- Dates/month.py
+++ Dates/month.py
@@ -2,9 +2,9 @@
 
 import calendar
 
-yy = int(input("Indiquez une année : "))
-mm = int(input("Indiquez un mois (entre 1 et 12): "))
+year = int(input("Indiquez une année : "))
+month = int(input("Indiquez un mois (entre 1 et 12): "))
 
 # affiche le calendrier
 
-print(calendar.month(yy,mm))
+print(calendar.month(year,month))
